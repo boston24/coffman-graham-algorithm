5a8092dd480d37812eb98dfaf3b270f1bd4af9d9
Mateusz Jendernal <matij99@gmail.com>
2021-01-06 22:20:36 +0100

    added labeling of last nodes

--- main.py
+++ main.py
@@ -11,19 +11,52 @@ import plotly.express as px
 def start():
     n = len(list)
     iteration = 1
+    iteration_list = []
+    iteration_list_temp = []
 
-    if iteration == 1: 
-        iteration_list = makeListStart
-    else:
-        iteration_list = makeList
+    while iteration<n:
+            
+        ##if iteration == 4:
+        ##    iteration = n
+
+        iteration_list_temp = iteration_list
+        iteration_list = []
+
+        if iteration == 1: 
+            iteration_list = makeListStart()
+
+        else:
+            for node in iteration_list_temp:
+                for kid in node.kids:
+                    check = 1
+                    for parent in kid.parents:
+                        if parent.label == None:
+                            check = 0
+                    if check == 1:
+                        iteration_list.append(kid)
+
+        iteration_list = sorted(iteration_list, key=lambda x: x.name)
+
+        print("Iteration no. "+str(iteration))
+        print("Tasks to label: ",end="[ ")
+        for task in iteration_list:
+            print(task.name, end=" ")
+        print("]")
+
+        for node in iteration_list:
+            node.label = iteration
+            print("Set label of "+str(node.name)+" with "+str(iteration))
+            iteration += 1
 
 def makeListStart():
     out = []
     for node in list:
         if not node.kids:
             out.append(node) 
+    return out
 
-graph = Graph(list)
+start()
 
-graph.showGraph([])      
+#graph = Graph(list)
+#graph.showGraph([]) 
 #graph.showTimeline(makeListForTimetable(list))      
